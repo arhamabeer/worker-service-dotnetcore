25eb05c1a8134a80ff21b32f7b3c2c62dea7af28
Arham Abeer Ahmed <arham.abeer@braincrop.net>
2023-08-25 17:25:30 +0500

    first commit

---
+++ UserUpdateService/Program.cs
@@ -0,0 +1,10 @@
+using UserUpdateService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
